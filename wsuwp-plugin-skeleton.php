b50a5bc0c17c62cf46f8200bb9468b03d8d22376
jeremyfelt <jeremy.felt@gmail.com>
2016-02-29 08:51:11 -0800

    Move the main class into its own file

--- wsuwp-plugin-skeleton.php
+++ wsuwp-plugin-skeleton.php
@@ -10,36 +10,7 @@ Text Domain: [Plugin Text Domain]
 Domain Path: /languages
 */
 
-class WSUWP_Plugin_Skeleton {
-	/**
-	 * @var WSUWP_Plugin_Skeleton
-	 */
-	private static $instance;
 
-	/**
-	 * Maintain and return the one instance. Initiate hooks when
-	 * called the first time.
-	 *
-	 * @since 0.0.1
-	 *
-	 * @return \WSUWP_Plugin_Skeleton
-	 */
-	public static function get_instance() {
-		if ( ! isset( self::$instance ) ) {
-			self::$instance = new WSUWP_Plugin_Skeleton();
-			self::$instance->setup_hooks();
-		}
-		return self::$instance;
-	}
-
-	/**
-	 * Setup hooks to include.
-	 *
-	 * @since 0.0.1
-	 */
-	public function setup_hooks() {}
-
-}
 
 add_action( 'after_setup_theme', 'WSUWP_Plugin_Skeleton' );
 /**
