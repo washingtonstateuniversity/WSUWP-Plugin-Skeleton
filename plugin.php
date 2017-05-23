fb8b8cfe22b7c91f52e3e356c1d26c18563c3d25
Jeremy Felt <jeremy.felt@gmail.com>
2017-05-23 12:17:04 -0700

    Update structure and documentation for new project structure
    This enforces a minimum PHP version of 5.3 so that we can use
    namespaces and avoid classes when they do not actually represent
    an object.

--- plugin.php
+++ plugin.php
@@ -5,9 +5,8 @@ Version: 0.0.1
 Description: A skeleton project to use when starting a new WSU WordPress plugin. Replace this description.
 Author: washingtonstateuniversity, [Other Plugin Authors]
 Author URI: https://web.wsu.edu/
-Plugin URI: https://github.com/washingtonstateuniversity/WSUWP-Plugin-Skeleton
-Text Domain: [Plugin Text Domain]
-Domain Path: /languages
+Plugin URI: https://github.com/washingtonstateuniversity/wsuwp-plugin-skeleton
+Text Domain: wsuwp-plugin-skeleton
 */
 
 // If this file is called directly, abort.
@@ -15,15 +14,11 @@ if ( ! defined( 'WPINC' ) ) {
 	die;
 }
 
-// The core plugin class.
-require dirname( __FILE__ ) . '/includes/class-wsuwp-plugin-skeleton.php';
-
-add_action( 'after_setup_theme', 'WSUWP_Plugin_Skeleton' );
-/**
- * Start things up.
- *
- * @return \WSUWP_Plugin_Skeleton
- */
-function WSUWP_Plugin_Skeleton() {
-	return WSUWP_Plugin_Skeleton::get_instance();
+// This plugin uses namespaces and requires PHP 5.3 or greater.
+if ( version_compare( PHP_VERSION, '5.3', '<' ) ) {
+	add_action( 'admin_notices', create_function( '',
+	"echo '<div class=\"error\"><p>" . __( 'WSUWP Plugin Skeleton requires PHP 5.3 to function properly. Please upgrade PHP or deactivate the plugin.', 'wsuwp-plugin-skeleton' ) . "</p></div>';" ) );
+	return;
+} else {
+	include_once __DIR__ . '/includes/wsuwp-plugin-skeleton.php';
 }
